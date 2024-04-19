9787a0546da226eafea820257fa219f16c0dd071
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-04-19 13:02:03 +0600

    task

--- backend/src/App.jsx
+++ backend/src/App.jsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [user,serUser] = useState([]);
+  const [users,serUser] = useState([]);
 
   useEffect(()=>{
-fetch("http://localhost:5000/user")
+fetch("http://localhost:5000/users")
 .then(res => res.json())
 .then(data => serUser(data))
   },[])
@@ -18,6 +17,20 @@ const name = form.name.value;
 const email = form.email.value;
 const user = {name,email}
 console.log(user);
+fetch("http://localhost:5000/users",{
+  method: 'POST',
+  headers:{
+'content-type': 'application/json'
+  },
+  body:JSON.stringify(user)
+})
+.then(res => res.json())
+.then(data => {
+  
+  const newUsers = [...users,data]
+  serUser(newUsers)
+  form.reset()
+})
   }
 
   return (
@@ -25,7 +38,7 @@ console.log(user);
    
       
     <h1>nazmul islam jewel</h1>
-    <h3>data length : {user.length}</h3>
+    <h3>data length : {users.length}</h3>
   <div>
     <form onSubmit={handleClick}>
       <input type="name" name="name" placeholder='name' />
@@ -33,6 +46,11 @@ console.log(user);
       <input type="email" name="email" placeholder='email' />
       <button>click for submit</button>
     </form>
+    {users.map(data => (
+  <div key={data.id}>
+    {data.name}: {data.email}
+  </div>
+))}
   </div>
     
     </div>
