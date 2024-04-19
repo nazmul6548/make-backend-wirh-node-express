d1c9b637a81d94cad090019c520ac45cea18d60b
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-04-19 12:03:27 +0600

    tasl

--- backend/src/App.jsx
+++ backend/src/App.jsx
@@ -11,16 +11,31 @@ fetch("http://localhost:5000/user")
 .then(res => res.json())
 .then(data => serUser(data))
   },[])
+  const handleClick = e => {
+e.preventDefault();
+const form = e.target;
+const name = form.name.value;
+const email = form.email.value;
+const user = {name,email}
+console.log(user);
+  }
 
   return (
-    <>
+    <div className='text-center'>
    
       
     <h1>nazmul islam jewel</h1>
     <h3>data length : {user.length}</h3>
-  
+  <div>
+    <form onSubmit={handleClick}>
+      <input type="name" name="name" placeholder='name' />
+      <br />
+      <input type="email" name="email" placeholder='email' />
+      <button>click for submit</button>
+    </form>
+  </div>
     
-    </>
+    </div>
   )
 }
 
