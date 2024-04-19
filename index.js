d4918764081dae89b519a0aeb6bcd50a8bc0f0d0
nazmul jewel <nazmul.islam.juwel9@gmail.com>
2024-04-19 10:27:37 +0600

    task

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 
+const user = [
+    {
+        id:1,name:"jewel",email:"nazmul@gmail.com"
+    }
+]
+
 app.get('/', (req, res) => {
     res.send('Hello World! hello jewel;')
+  })
+
+
+  app.get("/user",(req, res) => {
+    res.send(user)
   })
 
   app.listen(port, () => {
